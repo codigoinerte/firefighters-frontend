9ad9600911280c840a2f90b9ad1ffab87a2ee4ca
codigoinerte <fmartinez.bpe@gmail.com>
2025-08-29 17:18:39 -0500

    feat: agregar nuevos componentes de filtro y actualizar exportaciones en index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,18 @@
-export * from './Mapa'
-export * from './Header'
+// eslint-disable-next-line react-refresh/only-export-components
+export * from './customadvancedmarker'
+// eslint-disable-next-line react-refresh/only-export-components
+export * from './custompin'
+// eslint-disable-next-line react-refresh/only-export-components
+export * from './filteritemselect'
+// eslint-disable-next-line react-refresh/only-export-components
+export * from './filters'
+// eslint-disable-next-line react-refresh/only-export-components
+export * from './header'
+// eslint-disable-next-line react-refresh/only-export-components
+export * from './loader'
+// eslint-disable-next-line react-refresh/only-export-components
+export * from './mapa'
+// eslint-disable-next-line react-refresh/only-export-components
+export * from './header'
+// eslint-disable-next-line react-refresh/only-export-components
+export * from './MapaAdvance'
