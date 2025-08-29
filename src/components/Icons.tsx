9abec42b5abbbc02da020d75b82c7caea8f28773
codigoinerte <fmartinez.bpe@gmail.com>
2025-08-29 17:19:17 -0500

    feat: agregar propiedad fill al componente Icons para manejo de color de relleno

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,10 +1,12 @@
 import { HugeiconsIcon } from '@hugeicons/react'
 import { IconsProps } from '../interface'
 
-export const Icons = ({ icon, size=16, color="#000", strokeWidth=1.5 }:IconsProps) => {
+export const Icons = ({ icon, size=16, color="#000", strokeWidth=1.5, fill = undefined }:IconsProps) => {
     return <HugeiconsIcon 
         icon={icon} 
         size={size} 
         color={color}
-        strokeWidth={strokeWidth} />
+        strokeWidth={strokeWidth} 
+        {...(fill!== undefined ? {fill}: {})}
+        />
 }
