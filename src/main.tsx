e5b2ab5fc36514dda43a67b6ba7831f380e24380
codigoinerte <fmartinez.bpe@gmail.com>
2025-08-29 17:17:53 -0500

    feat: agregar APIProvider para integrar Google Maps en la aplicación

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import '/public/css/main.css';
+import { APIProvider } from '@vis.gl/react-google-maps';
+
+const key = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>    
-    <App />
+  <React.StrictMode>
+    <APIProvider apiKey={key} libraries={['marker']} version='beta'>    
+      <App />
+    </APIProvider>
   </React.StrictMode>,
 )
