5a469598e88d4fed4840ab6ca5f72252d63991ec
codigoinerte <fmartinez.bpe@gmail.com>
2025-09-01 12:14:34 -0500

    feat: agregar mensaje de advertencia para el servidor gratuito en el componente Loader

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -4,12 +4,20 @@ import './Loader.css';
 interface LoaderProps {
     style: CSSProperties | undefined
 }
-
+const IS_CREE_TIER = import.meta.env.VITE_FREE_TIER;
 export const Loader = ({style}:LoaderProps) => {
     return (
         <div className="loader" style={style}>
             <img src={`/images/fire.gif`} alt="loading" title="loading" />
             <b>Cargando</b>
+            {
+                IS_CREE_TIER === 'true' &&
+                (
+                    <div className='message'>
+                        <p>⚠️ El servidor gratuito puede tardar en iniciar.</p>
+                    </div>
+                )
+            }
         </div>
     )
 }
